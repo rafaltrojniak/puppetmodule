43d208aaed203119221be2a1b5fef4ea1a6e25d6
Justin Cattle <j@ocado.com>
2018-02-12 11:31:35 +0000

    Make puppet_agent_major_version fact regex more reliable

--- lib/facter/puppet_agent_major_version.rb
+++ lib/facter/puppet_agent_major_version.rb
@@ -5,7 +5,7 @@ Facter.add('puppet_agent_major_version') do
     puppet_version = Facter.value("puppetversion")
 
     if puppet_version != nil
-      major_version = puppet_version.match(/^(\d+)\.\d+\.\d+/)
+      major_version = puppet_version.match(/^([0-9]+)\.[0-9]+\.[0-9]+$/)
       major_version[1] if major_version
     else
       nil
