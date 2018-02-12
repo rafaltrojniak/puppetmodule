8fd7a0b878d51e2eff3d301aec3f3b91c2eed510
Justin Cattle <j@ocado.com>
2018-02-12 11:53:09 +0000

    Make puppet_server_versions safe without puppetserver

--- lib/facter/puppet_server_versions.rb
+++ lib/facter/puppet_server_versions.rb
@@ -2,12 +2,18 @@ Facter.add('puppet_server_version') do
   confine :kernel => 'Linux'
   setcode do
 
-    version = Facter::Util::Resolution.exec('puppetserver --version 2>&1').
-      lines.
-      to_a.
-      select { |line| line.match(/^puppetserver version:/) }.
-      first.
-      rstrip
+    raw_version = Facter::Util::Resolution.exec('puppetserver --version 2>&1')
+
+    if raw_version != nil
+      version = Facter::Util::Resolution.exec('puppetserver --version 2>&1').
+        lines.
+        to_a.
+        select { |line| line.match(/^puppetserver version:/) }.
+        first.
+        rstrip
+    else
+      version = nil
+    end
 
     if version != nil
       version.gsub(/^puppetserver version: ([^ ]+).*$/, '\1')
